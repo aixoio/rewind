e3f51c8748e2f0d47a1768ade47fe706f68d597c
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-28 21:20:55 +1000

    add current branch command

--- src/git/repo.rs
+++ src/git/repo.rs
@@ -1,5 +1,16 @@
-use std::path::Path;
+use std::{path::Path, process::Command};
+
+use anyhow::Ok;
 
 pub fn is_git_repo() -> bool {
     Path::new(".git").exists()
+}
+
+pub fn current_branch() -> anyhow::Result<String> {
+    let output = Command::new("git")
+        .arg("branch")
+        .arg("--show-current")
+        .output()?;
+
+    Ok(String::from_utf8_lossy(&output.stdout).to_string())
 }
