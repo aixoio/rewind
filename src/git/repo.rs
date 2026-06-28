b46441757fee521a957a689faf5b13b460fe412b
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-28 23:09:24 +1000

    add add_files

--- src/git/repo.rs
+++ src/git/repo.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, process::Command};
 
-use anyhow::Ok;
+use anyhow::anyhow;
 
 pub fn is_git_repo() -> bool {
     Path::new(".git").exists()
@@ -16,4 +16,14 @@ pub fn current_branch() -> anyhow::Result<String> {
         .to_string()
         .trim()
         .to_string())
+}
+
+pub fn add_files(path: &String) -> anyhow::Result<()> {
+    let output = Command::new("git").arg("add").arg(path).output()?;
+
+    if !output.status.success() {
+        return Err(anyhow!("error: non success exit code from git"));
+    }
+
+    Ok(())
 }
