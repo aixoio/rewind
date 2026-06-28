62183142d06280f16b47c421845eac366d254fb1
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-28 21:05:17 +1000

    add owo-colors

--- src/cli/commands/status.rs
+++ src/cli/commands/status.rs
@@ -1,10 +1,11 @@
 use std::process;
 
 use crate::git::{repo::is_git_repo, status::fetch_status};
+use owo_colors::OwoColorize;
 
 pub fn run() {
     if !is_git_repo() {
-        eprintln!("Not a git repository");
+        eprintln!("{}", "Not a git repository".bright_red().bold());
         process::exit(1);
     }
 
