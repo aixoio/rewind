8aa13cbb03e883f14d46ad59c8f4a61cf7b8c18a
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-28 21:12:24 +1000

    add formatting and colors

--- src/cli/commands/status.rs
+++ src/cli/commands/status.rs
@@ -1,15 +1,40 @@
-use std::process;
-
 use crate::git::{repo::is_git_repo, status::fetch_status};
 use owo_colors::OwoColorize;
 
 pub fn run() {
     if !is_git_repo() {
         eprintln!("{}", "Not a git repository".bright_red().bold());
-        process::exit(1);
+        return;
     }
 
     let status = fetch_status().expect("failed to fetch git status");
 
-    println!("{status:#?}");
+    if status.total_files() == 0 {
+        println!("{}", "No changes to commit.".bright_black());
+        return;
+    }
+
+    if status.staged().len() != 0 {
+        println!("{}", "Staged:".bold());
+
+        for file in status.staged() {
+            println!("     {}", file);
+        }
+    }
+
+    if status.unstaged().len() != 0 {
+        println!("{}", "Unstaged:".bold());
+
+        for file in status.unstaged() {
+            println!("     {}", file);
+        }
+    }
+
+    if status.untracked().len() != 0 {
+        println!("{}", "Untracked:".bold());
+
+        for file in status.untracked() {
+            println!("     {}", file);
+        }
+    }
 }
