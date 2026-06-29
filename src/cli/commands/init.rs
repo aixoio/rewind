ffaa3cb21fd1e52b01c8405b8e869524dbe4755e
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-29 11:40:54 +1000

    add init command

--- src/cli/commands/init.rs
+++ src/cli/commands/init.rs
@@ -1,3 +1,13 @@
+use crate::git::repo::{init_repo, is_git_repo};
+
+use owo_colors::OwoColorize;
+
 pub fn run() {
-    println!("initing...");
+    if is_git_repo() {
+        println!("{}", "Reinitialized existing Git repository");
+    }
+
+    init_repo().expect("faild to initialize git repo");
+
+    println!("{}", "Initialized Git repository".bold());
 }
