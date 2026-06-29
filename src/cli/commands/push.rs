a4d8e5e9c81a14cda6cebdab1690a16aa6c5de29
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-29 14:07:10 +1000

    add push command

--- src/cli/commands/push.rs
+++ src/cli/commands/push.rs
@@ -1,3 +1,26 @@
+use crate::git::remote;
+
+use owo_colors::OwoColorize;
+
 pub fn run() {
-    println!("pushing..");
+    let upstream = remote::upstream();
+
+    if upstream.is_none() {
+        println!("{}", "No upstream branch configured".italic());
+
+        println!("{}", "Setting upstream to origin/HEAD".blue());
+
+        remote::push_set_upstream().expect("failed to set upstream");
+
+        println!("{}", "Push completed!".green().bold());
+        println!("{}", "Upstream set and changes pushed".bright_black());
+    }
+
+    if upstream.is_some() {
+        println!("{}", "Pushing changes to remote...".blue());
+
+        remote::push().expect("failed to push upstream");
+
+        println!("{}", "Push completed!".green().bold());
+    }
 }
