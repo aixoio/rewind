62c14da3cba700dd53315dbd7f670dfeedaa0fec
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-29 18:17:16 +1000

    add log::fetch_log()

--- src/git/log.rs
+++ src/git/log.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use anyhow::anyhow;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -21,6 +23,17 @@ impl Commit {
     getter!(date, String);
     getter!(refs, Vec<String>);
     getter!(subject, String);
+}
+
+pub fn fetch_log() -> anyhow::Result<Vec<Commit>> {
+    let output = Command::new("git")
+        .arg("log")
+        .arg("--pretty=format:%H%x1f%cI%x1f%D%x1f%s%x1e")
+        .output()?;
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+
+    parse_format_string(&stdout)
 }
 
 /// commits must follow the format git log --pretty=format:%H%x1f%cI%x1f%D%x1f%s%x1e
