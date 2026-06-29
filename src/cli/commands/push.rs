dd542f14ab8b4d2ee3e445edfd2f57c60aa47f6d
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-29 14:15:38 +1000

    refactor to use match

--- src/cli/commands/push.rs
+++ src/cli/commands/push.rs
@@ -3,24 +3,22 @@ use crate::git::remote;
 use owo_colors::OwoColorize;
 
 pub fn run() {
-    let upstream = remote::upstream();
+    match remote::upstream() {
+        Some(_) => {
+            println!("{}", "Pushing changes to remote...".blue());
 
-    if upstream.is_none() {
-        println!("{}", "No upstream branch configured".italic());
+            remote::push().expect("failed to push upstream");
 
-        println!("{}", "Setting upstream to origin/HEAD".blue());
+            println!("{}", "Push completed!".green().bold());
+        }
+        None => {
+            println!("{}", "No upstream branch configured".italic());
+            println!("{}", "Setting upstream to origin/HEAD".blue());
 
-        remote::push_set_upstream().expect("failed to set upstream");
+            remote::push_set_upstream().expect("failed to set upstream");
 
-        println!("{}", "Push completed!".green().bold());
-        println!("{}", "Upstream set and changes pushed".bright_black());
-    }
-
-    if upstream.is_some() {
-        println!("{}", "Pushing changes to remote...".blue());
-
-        remote::push().expect("failed to push upstream");
-
-        println!("{}", "Push completed!".green().bold());
-    }
+            println!("{}", "Push completed!".green().bold());
+            println!("{}", "Upstream set and changes pushed".bright_black());
+        }
+    };
 }
