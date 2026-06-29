7ec40fc91123161a0528a44b6a1987b97a73e69b
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-29 23:15:14 +1000

    bug fix

--- src/cli/commands/log.rs
+++ src/cli/commands/log.rs
@@ -32,7 +32,7 @@ pub fn run(limit: Option<usize>, show_all: bool) {
     println!();
 
     for commit in commits.iter().rev() {
-        let short_hash = &commit.hash()[..6];
+        let short_hash = &commit.hash()[..7];
 
         if commit.refs().is_empty() {
             println!("{} {}", short_hash.bright_black(), commit.subject().bold());
