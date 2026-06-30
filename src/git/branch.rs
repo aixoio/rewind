169debf9f204778e90116917ef07b81be5d49198
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-01 00:14:40 +1000

    add parse_branch_names function with tests

--- src/git/branch.rs
+++ src/git/branch.rs
@@ -8,3 +8,39 @@ pub fn current_branch() -> anyhow::Result<String> {
 
     Ok(String::from_utf8_lossy(&output.stdout).trim().to_string())
 }
+
+/// must be used with `git --no-pager branch --list --format='%(refname:short)'`
+fn parse_branch_names(stdout: &str) -> Vec<&str> {
+    stdout
+        .trim()
+        .lines()
+        .filter_map(|line| {
+            if line.trim().is_empty() {
+                return None;
+            }
+
+            Some(line.trim())
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_branch_names() {
+        let input = "master\nmain\nstaging";
+        let target = vec!["master", "main", "staging"];
+
+        assert_eq!(target, parse_branch_names(input))
+    }
+
+    #[test]
+    fn test_parse_branch_names_empty() {
+        let input = "\n\t\t\n";
+        let target: Vec<&str> = vec![];
+
+        assert_eq!(target, parse_branch_names(input))
+    }
+}
