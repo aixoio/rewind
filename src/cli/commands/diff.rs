b7fe4fa5681b8c9e4923bef42424d44add2ce84a
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-01 12:11:54 +1000

    bug fix diff

--- src/cli/commands/diff.rs
+++ src/cli/commands/diff.rs
@@ -1,5 +1,12 @@
-use crate::git::repo;
+use crate::git::repo::{self, is_git_repo};
+
+use owo_colors::OwoColorize;
 
 pub fn run() {
+    if !is_git_repo() {
+        eprintln!("{}", "Not a git repository".bright_red().bold());
+        return;
+    }
+
     repo::diff().expect("failed to diff");
 }
