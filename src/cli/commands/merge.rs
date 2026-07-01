c64fa288570962678745a82f3b6f04645ece2212
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-01 20:03:53 +1000

    check both branchs exist in merge command

--- src/cli/commands/merge.rs
+++ src/cli/commands/merge.rs
@@ -1,3 +1,27 @@
+use crate::git::branch::branch_exists;
+
+use owo_colors::OwoColorize;
+
 pub fn run(source: String, target: String) {
-    println!("merining {source} -> {target}");
+    if !branch_exists(&source) {
+        eprintln!(
+            "{} {} {}",
+            "branch".red(),
+            source.bright_red().bold(),
+            "does not exist".red()
+        );
+        return;
+    }
+
+    if !branch_exists(&target) {
+        eprintln!(
+            "{} {} {}",
+            "branch".red(),
+            target.bright_red().bold(),
+            "does not exist".red()
+        );
+        return;
+    }
+
+    println!("both branchs exist");
 }
