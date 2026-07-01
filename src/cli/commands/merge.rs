45d590dbcb50531f7461397ccc960f706165b53d
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-01 20:13:14 +1000

    make merge command merge 2 branchs with git merge and repo::merge

--- src/cli/commands/merge.rs
+++ src/cli/commands/merge.rs
@@ -1,4 +1,7 @@
-use crate::git::branch::branch_exists;
+use crate::git::{
+    branch::{branch_exists, current_branch},
+    repo::{checkout, merge},
+};
 
 use owo_colors::OwoColorize;
 
@@ -23,5 +26,31 @@ pub fn run(source: String, target: String) {
         return;
     }
 
-    println!("both branchs exist");
+    let Ok(current_branch) = current_branch() else {
+        eprintln!("{}", "cannot get current branch".red().bold());
+        return;
+    };
+
+    if current_branch.trim() != target.trim() {
+        println!("{} {}", "Switching to target branch".green(), target.cyan());
+        checkout(target.trim()).expect("cannot switch to target branch");
+    }
+
+    println!(
+        "{} {} {} {}",
+        "Merging".blue(),
+        source.cyan(),
+        "into".blue(),
+        target.cyan()
+    );
+
+    merge(source.trim()).expect("cannnot merge branchs");
+
+    println!(
+        "{} {} {} {}",
+        "Merged".green(),
+        source.bright_green().bold(),
+        "->".green(),
+        target.bright_green().bold()
+    );
 }
