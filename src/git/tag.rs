7501963063d33c4df81ee92f2f6fc382f20eecb2
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-02 21:11:21 +1000

    add a struct for tags with lifetimes

--- src/git/tag.rs
+++ src/git/tag.rs
@@ -1,5 +1,26 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Tag<'a> {
+    name: &'a str,
+    relative_date: &'a str,
+    subject: &'a str,
+}
+
+macro_rules! getter {
+    ($name:ident, $type:ty) => {
+        pub fn $name(&self) -> &$type {
+            &self.$name
+        }
+    };
+}
+
+impl<'a> Tag<'a> {
+    getter!(name, &'a str);
+    getter!(relative_date, &'a str);
+    getter!(subject, &'a str);
+}
+
 /// input must be from the `git --no-pager tag -l --sort=-creatordate --format='%(refname:short)%1f%(creatordate:relative)%1f%(subject)%1e'` command
-fn parse_git_tags(format_string: &str) -> Vec<&str> {
+fn parse_git_tags<'a>(format_string: &'a str) -> Vec<Tag<'a>> {
     vec![]
 }
 
