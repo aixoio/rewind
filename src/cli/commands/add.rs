f5e9be446b564ae41f5b65601bb1dda5d5afc79a
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-03 11:42:14 +1000

    add better error handling

--- src/cli/commands/add.rs
+++ src/cli/commands/add.rs
@@ -2,6 +2,15 @@ use crate::git::repo::{add_paths, is_git_repo};
 
 use owo_colors::OwoColorize;
 
+macro_rules! handle_error {
+    ($ex:expr) => {
+        if let Err(err) = $ex {
+            println!("{} {}", "error:".bright_red().bold(), err.bold());
+            return;
+        };
+    };
+}
+
 pub fn run(paths: Option<Vec<String>>) {
     if !is_git_repo() {
         eprintln!("{}", "Not a git repository".bright_red().bold());
@@ -10,7 +19,7 @@ pub fn run(paths: Option<Vec<String>>) {
 
     let paths = paths.unwrap_or_else(|| vec![".".to_string()]);
 
-    add_paths(&paths).expect("cannot add paths");
+    handle_error!(add_paths(&paths));
 
     println!("{}", "Files staged successfully!".green().bold());
     println!("{}", "Ready for commit.".bright_black());
