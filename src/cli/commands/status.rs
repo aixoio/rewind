c5fcf9cf08670db6f74063f2b822952511c217bb
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-03 12:10:30 +1000

    use macro and better error handling in status command

--- src/cli/commands/status.rs
+++ src/cli/commands/status.rs
@@ -1,22 +1,33 @@
-use crate::git::{
-    branch::current_branch,
-    repo::is_git_repo,
-    status::{fetch_status, parse_status},
+use crate::{
+    check_for_git_repo,
+    git::{
+        branch::current_branch,
+        status::{fetch_status, parse_status},
+    },
 };
 use owo_colors::OwoColorize;
 
 pub fn run() {
-    if !is_git_repo() {
-        eprintln!("{}", "Not a git repository".bright_red().bold());
-        return;
-    }
+    check_for_git_repo!();
 
-    let branch = current_branch().expect("cannot get current branch");
+    let branch = match current_branch() {
+        Ok(branch) => branch,
+        Err(err) => {
+            eprintln!("{} {}", "error:".bright_red().bold(), err.bold());
+            return;
+        }
+    };
 
     println!("On branch {}", branch.bold());
     println!();
 
-    let status = fetch_status().expect("failed to fetch git status");
+    let status = match fetch_status() {
+        Ok(status) => status,
+        Err(err) => {
+            eprintln!("{} {}", "error:".bright_red().bold(), err.bold());
+            return;
+        }
+    };
     let status = parse_status(&status);
 
     if status.total_files() == 0 {
