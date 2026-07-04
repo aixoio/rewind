4c3a102960276c481a5f30782b0026c3b85763a0
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-04 22:09:44 +1000

    display commit info in revert command

--- src/cli/commands/revert.rs
+++ src/cli/commands/revert.rs
@@ -1,11 +1,28 @@
 use std::process::ExitCode;
 
-use crate::check_for_git_repo;
+use crate::{
+    check_for_git_repo,
+    git::commits::{fetch_commit_info, parse_commit_info},
+    return_error,
+};
 
 pub fn run(hash: String) -> ExitCode {
     check_for_git_repo!();
 
-    println!("revertting {hash}");
+    let commit_info = match fetch_commit_info(&hash) {
+        Ok(commit_info) => commit_info,
+        Err(err) => {
+            return_error!(err);
+        }
+    };
+    let commit_info = match parse_commit_info(&commit_info) {
+        Some(commit_info) => commit_info,
+        None => {
+            return_error!("cannot parse commit info");
+        }
+    };
+
+    println!("{commit_info:?}");
 
     ExitCode::SUCCESS
 }
