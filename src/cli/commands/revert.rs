3b27c4e7545439e169789d29496fd222580a9775
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-04 22:30:53 +1000

    disply the commit to revert

--- src/cli/commands/revert.rs
+++ src/cli/commands/revert.rs
@@ -5,6 +5,7 @@ use crate::{
     git::commits::{fetch_commit_info, parse_commit_info},
     return_error,
 };
+use owo_colors::OwoColorize;
 
 pub fn run(hash: String) -> ExitCode {
     check_for_git_repo!();
@@ -22,7 +23,15 @@ pub fn run(hash: String) -> ExitCode {
         }
     };
 
-    println!("{commit_info:?}");
+    println!("{}", "Commit to revert:".blue().bold());
+    println!(
+        "   {} {}",
+        "Author:".bright_blue().bold(),
+        commit_info.author()
+    );
+    println!("   {} {}", "Date:".bright_blue().bold(), commit_info.date());
+    println!("     {}", commit_info.hash().bright_black());
+    println!("     {}", commit_info.message());
 
     ExitCode::SUCCESS
 }
