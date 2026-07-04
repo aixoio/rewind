ab762b5be76508b7515651a24d2708d0e3346331
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-04 21:53:27 +1000

    add commits::parse_commit_info

--- src/git/commits.rs
+++ src/git/commits.rs
@@ -14,3 +14,20 @@ impl CommitInfo {
     getter!(author, String);
     getter!(date, String);
 }
+
+/// input must come from `git show --no-patch --format=%H%x1f%s%x1f%an%x1f%ad --date=short`
+pub fn parse_commit_info(input: &str) -> Option<CommitInfo> {
+    let mut data = input.trim().split('\x1f');
+
+    let hash = data.next()?.to_string();
+    let message = data.next()?.to_string();
+    let author = data.next()?.to_string();
+    let date = data.next()?.to_string();
+
+    Some(CommitInfo {
+        hash,
+        message,
+        author,
+        date,
+    })
+}
