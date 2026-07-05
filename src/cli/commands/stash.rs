9ef9a85957c7752da173f128bf556ef2209c0d8c
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-05 22:50:24 +1000

    add stash command sub command placeholder functions

--- src/cli/commands/stash.rs
+++ src/cli/commands/stash.rs
@@ -13,8 +13,24 @@ pub enum StashCommands {
 pub fn run(sub_command: Option<StashCommands>) -> ExitCode {
     check_for_git_repo!();
 
-    println!("Stash");
-    println!("{sub_command:?}");
+    match sub_command {
+        None => stash_create(),
+        Some(StashCommands::List) => stash_list(),
+        Some(StashCommands::Pop) => stash_pop(),
+    }
+}
 
+fn stash_create() -> ExitCode {
+    println!("stash create");
+    ExitCode::SUCCESS
+}
+
+fn stash_list() -> ExitCode {
+    println!("stash list");
+    ExitCode::SUCCESS
+}
+
+fn stash_pop() -> ExitCode {
+    println!("stash pop");
     ExitCode::SUCCESS
 }
